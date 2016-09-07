ce3dab0b6c8b72520027f15fefabed4c85113267
AbelZong <zongq890630@gmail.com>
2016-09-07 14:36:25 +0800

    git push

---
+++ CoreData/MySqlData.cs
@@ -0,0 +1,16 @@
+﻿using CoreModels.XyUser;
+using MySql.Data.MySqlClient;
+using Dapper;
+using System.Collections.Generic;
+
+namespace CoreData
+{
+    public static class MySqlData
+    {
+        public static List<User> GetData()
+        {    
+            MySqlConnection con = new MySqlConnection("server=xieyuntestout.mysql.rds.aliyuncs.com;database=xyuser;uid=xieyun;pwd=xieyun123;Port=3306;SslMode=None");
+            return con.Query<User>("select * from user limit 10").AsList();
+        }
+    }
+}
