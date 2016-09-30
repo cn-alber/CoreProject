d2832cb8d0d066f2ae934741883383929df8fa58
zoezhang007 <406560781@qq.com>
2016-09-30 16:32:36 +0800

    Add User(Insert,Update,Cache,Enable,Query)

--- CoreModels/XyUser/User.cs
+++ CoreModels/XyUser/User.cs
@@ -153,8 +153,8 @@ namespace CoreModels.XyUser
         public bool Enable { get; set; }
         public string Email { get; set; }
         public string Gender { get; set; }
-        public int CompanyName { get; set; }
-        public int RoleName { get; set; }
+        public string CompanyName { get; set; }
+        public string RoleName { get; set; }
         public DateTime CreateDate { get; set; }
     }
     #endregion
@@ -178,4 +178,27 @@ namespace CoreModels.XyUser
 		public List<UserQuery> UserLst {get;set;}//返回查询结果
 	}
 	#endregion
+
+    #region
+    public class UserEdit
+    {
+        public int ID{get;set;}
+        public string Account{get;set;}
+        public string SecretID{get;set;}
+        public string Name{get;set;}
+        public string PassWord{get;set;}
+        public bool Enable{get;set;}
+        public string Email{get;set;}
+        public string Gender{get;set;}
+        public string Mobile{get;set;}
+        public string QQ{get;set;}
+        public int CompanyID{get;set;}
+        public int RoleID{get;set;}
+        public string Creator{get;set;}
+        public DateTime CreateDate{get;set;}
+        public string CompanyName { get; set; }
+        public string RoleName { get; set; }
+    }
+
+    #endregion
 }
