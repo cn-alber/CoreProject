c3f8b1bc8e189cfaa5931cade7e8d26293b4fbb9
hellobbq <yahveh.worker@gmail.com>
2016-10-06 15:38:43 +0800

    null

--- CoreWebApi/Controllers/Base/CommonControllers.cs
+++ CoreWebApi/Controllers/Base/CommonControllers.cs
@@ -5,8 +5,8 @@ namespace CoreWebApi
 {
     public class CommonController : ControllBase
     {
-        [HttpGetAttribute("/Core/Common/ScoCompany")]
-        public ResponseResult GetScoCompany()
+        [HttpGetAttribute("/Core/Common/ScoCompanySimple")]
+        public ResponseResult GetScoCompanySimple()
         {   
             int CoId = int.Parse(GetCoid());
             var data = ScoCompanyHaddle.GetScoCompanyAll(CoId);
